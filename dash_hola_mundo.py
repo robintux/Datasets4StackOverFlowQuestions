c2adfdc0eca084ad0d12f35de166d74bf7c6f56c
Abraham Zamudio Chauca <robintux@users.noreply.github.com>
2025-04-24 19:58:24 -0500

    Add files via upload

---
+++ dash_hola_mundo.py
@@ -0,0 +1,28 @@
+from dash import Dash, html
+import pandas as pd
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H4(children='US Agriculture Exports (2011)'),
+    generate_table(df)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8080, host="0.0.0.0")
